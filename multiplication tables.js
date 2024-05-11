1beb90362c5882ea9de627260dbd99d684c5c762
Farahsaiza <farahsaiza@gmail.com>
2024-05-12 00:48:18 +0100

    Create multiplication tables.js

---
+++ multiplication tables.js
@@ -0,0 +1,7 @@
+ for (let i = 1 ; i<=10 ; i++){
+            document.write("multiplication du nombre" + i+":<BR>")
+            for (let u = 0 ; u<=10 ; u++)
+            {
+                document.write(i + "*"  +  u + "  = " +i*u+"<BR>");
+            }
+        }
